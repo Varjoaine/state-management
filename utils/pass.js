5801ad805e9b2723834763ee504069eac9ea400d
Hanna Hoffren <hanna.hoffren@myy.haaga-helia.fi>
2019-11-21 12:03:35 +0200

    viesti

---
+++ utils/pass.js
@@ -0,0 +1,79 @@
+'use strict';
+   const passport = require('passport');
+   const Strategy = require('passport-local').Strategy;
+   
+   // fake database: ****************
+   const users = [
+     {
+       user_id: 1,
+       name: 'Foo Bar',
+       email: 'foo@bar.fi',
+       password: 'foobar',
+     },
+     {
+       user_id: 2,
+       name: 'Bar Foo',
+       email: 'bar@foo.fi',
+       password: 'barfoo',
+     },
+   ];
+   // *******************
+
+   // fake database functions *********
+    const getUser = (id) => {
+      const user = users.filter((usr) => {
+        if (usr.user_id === id) {
+          return usr;
+        }
+      });
+      return user[0];
+    };
+    
+    const getUserLogin = (email, pass) => {
+      const user = users.filter((usr) => {
+        if (usr.email === email && usr.password === pass) {
+          return usr;
+        }
+      });
+      return user[0];
+    };
+    // *****************
+    
+   // serialize: store user id in session 
+   passport.serializeUser((id, done) => {
+     console.log('serialize', id);
+     // serialize user id by adding it to 'done()' callback
+     done(null, id);
+   });
+   
+   // deserialize: get user id from session and get all user data
+   passport.deserializeUser( (id, done) => {
+       // get user data by id from getUser
+       const user = getUser(id);
+       console.log('deserialize', user);
+       
+       // deserialize user by adding it to 'done()' callback
+       return done(null, user);
+    });
+   
+   
+   
+   passport.use(new Strategy(
+       (username, password, done) => {
+                    // get user by username and password from getUserLogin
+         const user = getUserLogin(username, password);
+         // if user is undefined 
+         console.log(user);
+         if (user == undefined) 
+            return done(null, false);
+          
+         // return done(null, false);
+         return done(null, user.user_id);
+        
+         // else
+         // return done(null, user.user_id);
+       }
+   ));
+
+   
+   module.exports = passport;
